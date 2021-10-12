82e75ffb87bc06f33f46040a4efce0701316e297
EC2 Default User <ec2-user@ip-172-31-10-168.eu-north-1.compute.internal>
2021-10-12 08:08:30 +0000

    new huge exciting update

--- static_website.tf
+++ static_website.tf
@@ -1,8 +1,8 @@
 module "static-site" {
   source            = "telia-oss/static-site/aws"
   version           = "3.0.0"
-  bucket_versioning = true
+  bucket_versioning = false
   hosted_zone_name  = "thecloudcollege.com"
-  name_prefix       = "glenn"
-  site_name         = "glenn.thecloudcollege.com"
+  name_prefix       = "${var.user_name}"
+  site_name         = "${var.user_name}.thecloudcollege.com"
 }
